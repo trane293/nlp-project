6ea8e1a038bf762f7029f42e3a2d352c4154e47e
Amirali <amiralis@sfu.ca>
2017-10-02 01:18:40 -0700

    Add DP algorithem

--- segmenter/default.py
+++ segmenter/default.py
@@ -1,5 +1,10 @@
 
 import sys, codecs, optparse, os
+from heapq import heappush, heappop
+from collections import namedtuple
+
+# import sys
+sys.setrecursionlimit(3500)
 
 optparser = optparse.OptionParser()
 optparser.add_option("-c", "--unigramcounts", dest='counts1w', default=os.path.join('data', 'count_1w.txt'), help="unigram counts")
@@ -7,6 +12,7 @@ optparser.add_option("-b", "--bigramcounts", dest='counts2w', default=os.path.jo
 optparser.add_option("-i", "--inputfile", dest="input", default=os.path.join('data', 'input'), help="input file to segment")
 (opts, _) = optparser.parse_args()
 
+# Reading the counts1w and make a dictionary of words with their frequencies
 class Pdist(dict):
     "A probability distribution estimated from counts in datafile."
 
@@ -36,8 +42,48 @@ old = sys.stdout
 sys.stdout = codecs.lookup('utf-8')[-1](sys.stdout)
 # ignoring the dictionary provided in opts.counts
 with open(opts.input) as f:
+
+    # data = list()
+    Entry = namedtuple('Entry', 'word start logp back')
+    PHeap = []
+    chart = dict()
+
     for line in f:
         utf8line = unicode(line.strip(), 'utf-8')
-        output = [i for i in utf8line]  # segmentation is one word per character in the input
-        print " ".join(output)
+        print utf8line
+
+        # Step 1:
+        # Initializing step 
+        # Finding each word that matches input at position 0
+        for key in Pw:
+            if utf8line.find(key, 0) != -1 :
+                heappush(PHeap, Entry(word=key, start=0, logp=Pw(key), back=None))
+
+        # Step 2:
+        while PHeap:
+            head = heappop(PHeap)
+            print len(PHeap)
+
+            endindex = head.start + len(head.word) - 1
+
+            if endindex in chart:
+                preventry = chart[endindex]
+
+                if head.logp > preventry.logp:
+                    chart[endindex] = head
+            else:
+                chart[endindex] = head
+
+            for key in Pw:
+                if utf8line.find(key, endindex+1) != -1 :
+                    heappush(PHeap, Entry(word=key, start=endindex+1, logp=Pw(key), back=head))
+
+
+    finalindex = len(utf8line)
+    finalentry = chart[finalindex]
+    print finalentry
+
+        # output = [i for i in utf8line]  # segmentation is one word per character in the input
+        # print " ".join(output)
+
 sys.stdout = old

6baf93aedec454dee2fd0271193c521cc9089a76
asa224 <asa224@cs-mial-31.cs.sfu.ca>
2017-10-03 17:17:11 -0700

    define problem statement for LSTM, prepared dataset for training B-LSTM, defined model

--- segmenter/default_edited.py
+++ segmenter/default_edited.py
@@ -1,4 +1,9 @@
+
 import sys, codecs, optparse, os
+from heapq import heappush, heappop
+from collections import namedtuple
+
+# import sys sys.setrecursionlimit(3500)
 
 optparser = optparse.OptionParser()
 optparser.add_option("-c", "--unigramcounts", dest='counts1w', default=os.path.join('data', 'count_1w.txt'), help="unigram counts")
@@ -6,11 +11,12 @@ optparser.add_option("-b", "--bigramcounts", dest='counts2w', default=os.path.jo
 optparser.add_option("-i", "--inputfile", dest="input", default=os.path.join('data', 'input'), help="input file to segment")
 (opts, _) = optparser.parse_args()
 
+# Reading the counts1w and make a dictionary of words with their frequencies
 class Pdist(dict):
     "A probability distribution estimated from counts in datafile."
 
     def __init__(self, filename, sep='\t', N=None, missingfn=None):
-        self.maxlen = 0 
+        self.maxlen = 0
         for line in file(filename):
             (key, freq) = line.split(sep)
             try:
@@ -35,8 +41,16 @@ old = sys.stdout
 sys.stdout = codecs.lookup('utf-8')[-1](sys.stdout)
 # ignoring the dictionary provided in opts.counts
 with open(opts.input) as f:
+
+    # data = list()
+    Entry = namedtuple('Entry', 'start word logp back')
+    PHeap = []
+    chart = dict()
+
     for line in f:
         utf8line = unicode(line.strip(), 'utf-8')
+
         output = [i for i in utf8line]  # segmentation is one word per character in the input
         print " ".join(output)
+
 sys.stdout = old
